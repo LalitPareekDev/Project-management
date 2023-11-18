43126abcd0b51011efd6d9e15bae5c53323dc982
LalitPareekDev <114168059+LalitPareekDev@users.noreply.github.com>
2023-11-18 19:28:15 +0530

    Update production.js

--- src/config/production.js
+++ src/config/production.js
@@ -2,7 +2,7 @@
 const connect_db = {
     'mongo_host' : '127.0.0.1',
     'mongo_port' : '27017',
-    'mongo_db' : 'doctor_db'
+    'mongo_db' : 'project_m_s'
 }
 const jwtSecretKey = "sgjhgf7fgdfgdfgy897rtihgrworkgnfgxbcvbcndwerwhhfhncvxhkmfhjfgjsdhfbgjshvgjhdfsjhgjhsdfgjjsdhfgdsjfhdgsfjhhjfgshgfs";
 module.exports = { connect_db, jwtSecretKey };
